f21f7b1835b6bb46d3cf58187c4576113ce27cd7
FujiX55 <hnishi2000@gmail.com>
2017-02-26 12:13:13 +0900

    管理開始

---
+++ Assets/Scripts/Faucet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Faucet : MonoBehaviour {
+	    public GameObject waterPrefab;
+	    public int volume = 0;
+	    // Use this for initialization
+	    void Start () {
+		    
+		    }
+	    
+	    // Update is called once per frame
+	    void Update () {
+		    
+		        for (int i = 0; i < (volume*Time.deltaTime); i++) {
+			            Instantiate(waterPrefab, transform.position, transform.rotation);
+			        }
+		    }
+}
